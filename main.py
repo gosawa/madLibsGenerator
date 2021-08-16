015db8a7bf80f54ef9d676253a2f0de8b2b32b80
Goro Osawa <gosawa@gmail.com>
2021-08-16 17:40:07 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def madlibsgen():
+    If you've ever met my {}, Snoopy, then you know he\'s not
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
