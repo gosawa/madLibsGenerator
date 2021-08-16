3784af6fae6f112d09bbf62108175a943da6c6fe
Goro Osawa <gosawa@gmail.com>
2021-08-16 19:01:02 +0900

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-def madlibsgen():
-    If you've ever met my {}, Snoopy, then you know he\'s not
+class MadLibsGen:
+    def __init__(self):
+        self.theAnswers = []
+        self.thePrompts = ["animal", "adjective", "adjective", "adjective", "noun"]
+
+    def get_answers(self):
+
+        for i in self.thePrompts:
+            self.theAnswers.append(input(i))
+
+    def print_my_lib(self):
+        _myLib = f"""If you've ever met my {self.theAnswers[0]} Snoopy, then you know he's not your 
+        average canine companion. Some kids might find it {self.theAnswers[1]} that their 
+        beagle has such a/an {self.theAnswers[2]} imagination, but not me! All 
+        I've ever wanted was a normal, {self.theAnswers[3]} dog. Why can't I have 
+        a/an {self.theAnswers[4]} just like everyone else?"""
+        print(_myLib)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    my_lib = MadLibsGen()
+    my_lib.get_answers()
+    my_lib.print_my_lib()
